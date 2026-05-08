7141e876858185b23c2a2034288d97f8a39a0b55
Teja <114237349+Bala-Sai-Teja-Singh@users.noreply.github.com>
2026-05-09 04:45:20 +0530

    fix/navbar hiding issue fixed

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -6,7 +6,7 @@ import { MobileNav } from '@/components/layout/mobile-nav';
 
 export default function UserLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col min-h-screen pb-24 md:pb-0">
+    <div className="flex flex-col min-h-screen pt-16 pb-24 md:pt-0 md:pb-0">
       <Navbar />
       <main className="flex-1">
         {children}
